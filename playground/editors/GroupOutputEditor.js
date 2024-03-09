63fe3fc565829edcdcd20412b3fa261d2daa1648
abc013 <abc013@alpenjodel.de>
2024-03-09 18:48:50 +0100

    Add group output type selection

--- playground/editors/GroupOutputEditor.js
+++ playground/editors/GroupOutputEditor.js
@@ -1,30 +1,93 @@
+import { Element, SelectInput } from 'flow';
 import { BaseNodeEditor } from '../BaseNodeEditor.js';
-import { createElementFromJSON, getColorFromType, onValidType } from '../NodeEditorUtils.js';
+import { createElementFromJSON, onValidNode } from '../NodeEditorUtils.js';
+import { setInputAestheticsFromType } from '../DataTypeLib.js';
 
 export class GroupOutputEditor extends BaseNodeEditor {
 
 	constructor() {
-		const inputType = 'float';
 
 		super( 'Group Output', null, 350 );
+	
+		const types = [
+			{ name: "string", value: "string" },
+			{ name: "float", value: "float" },
+			{ name: "vec2", value: "vec2" },
+			{ name: "vec3", value: "vec3" },
+			{ name: "vec4", value: "vec4" },
+			{ name: "color", value: "color" },
+			{ name: "boolean", value: "bool" },
+			{ name: "anything", value: "any" },
+			{ name: "nothing", value: "null" }
+		];
+
+		const type = "any";
+
+		const typeInput = new SelectInput().onChange( () => {
+
+			this.generateInput( typeInput.getValue() );
+
+		} );
+
+		this.typeInput = typeInput;
+
+		this.add( new Element().add( typeInput ) );
+
+		typeInput.setOptions( types );
+		typeInput.setValue( type );
+
+	}
+
+	generateInput( type ) {
+
+		if ( type == this.inputType ) return;
+
+		let linkedObject = null;
+		if ( this.input ) {
+
+			linkedObject = this.input.getLinkedElement();
+			this.input.connect();
+
+			this.remove( this.input );
+
+			this.input = null;
+			this.inputNode = null;
+
+		}
+
+		if ( type == "null" ) {
+
+			this.updateConnection();
+			return;
+
+		}
 
 		const { element, inputNode } = createElementFromJSON( {
 			name: 'Output',
-			inputType: inputType,
+			inputType: type,
 			inputConnection: false
 		} );
 
-		element.setInput(1); // TODO: somehow, using inputConnection == true doesn't work, but this does...
-
-		this.inputNode = inputNode;
 		this.input = element;
-		
-		element.onConnect( () => this.updateConnection(), true );
+		this.inputNode = inputNode;
 
-		this.output = inputNode;
-		element.addEventListener( 'changeInput', () => this.invalidate() );
+		element.onConnect( () => { this.updateConnection() }, true );
+		element.addEventListener( 'changeInput', () => { this.invalidate() } );
+
+        setInputAestheticsFromType( element, type );
+		element.onValid( onValidNode );
+
 
 		this.add( element );
+
+		if ( linkedObject ) {
+
+			element.connect( linkedObject );
+
+		}
+
+		this.updateConnection();
+
 	}
 
     isRemovable() {
@@ -41,9 +104,17 @@ export class GroupOutputEditor extends BaseNodeEditor {
 
 	updateConnection() {
 
-		this.input.setEnabledInputs( ! this.input.getLinkedObject() );
-		
-		this.output = this.input.getLinkedObject() ?? this.inputNode; // TODO: change this.value here, its misleading.
+		if ( !this.input ) {
+
+			this.output = null;
+
+		} else {
+
+			this.input.setEnabledInputs( ! this.input.getLinkedObject() );
+			this.output = this.input.getLinkedObject() ?? this.inputNode;
+
+		}
+
 		if (this.parentGroupEditor) this.parentGroupEditor.updateOutputs();
 
 	}
@@ -55,4 +126,5 @@ export class GroupOutputEditor extends BaseNodeEditor {
 		if (this.parentGroupEditor) this.parentGroupEditor.invalidate();
 
 	}
+
 }
